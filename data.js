58204aba1428f1955a82fb9053ceb62de48d9513
Marcus Pousette <marcuspousette@gmail.com>
2023-01-11 09:38:37 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    title: 'Card with paragraph',
+    children: <p>I am a paragraph</p>,
+  },
+  {
+    title: 'Card with div',
+    children: <div>I am a div</div>,
+  },
+  {
+    title: 'Card with button',
+    children: <button>I am a button</button>,
+  },
+];
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+const Footer = () => {
+    return <div className="Footer">
+        <img className="Footer-logo" src="/logo.png" alt="Techover" />
+    </div>
+}
+
+export default Footer;
